e6b8a3c00f8188212f655d61e97a1027acdbb483
JSW0721 <stmjdi04@gmial.com>
2024-08-26 09:06:06 +0900

    modal test추가 및 js파일 추가

--- TASK1-Webpage/TASK1_script.js
+++ TASK1-Webpage/TASK1_script.js
@@ -1,19 +1,19 @@
-const modal = document.getElementById("modal");
-const openModalBtn = document.querySelector(".open-modal-btn");
-const closeModalBtn = document.querySelector(".close-btn");
+const modal = document.getElementById("modal"); //불러올 파라메터 모달 정의
+const openModalBtn = document.querySelector(".sub-btn");//모달을 불러올 오픈모달버튼 파라메터 정의
+const closeModalBtn = document.querySelector(".modal-close-btn");//모달을 닫을 클로즈모달버튼 파라메터 정의
 
 // 모달 열기
-openModalBtn.addEventListener("click", function() {
+openModalBtn.addEventListener('click', function() {
   modal.style.display = "block";
 });
 
 // 모달 닫기 (닫기 버튼 클릭 시)
-closeModalBtn.addEventListener("click", function() {
+closeModalBtn.addEventListener('click', function() {
   modal.style.display = "none";
 });
 
 // 모달 닫기 (모달 바깥쪽 클릭 시)
-window.addEventListener("click", function(event) {
+window.addEventListener('click', function(event) {
   if (event.target === modal) {
     modal.style.display = "none";
   }
